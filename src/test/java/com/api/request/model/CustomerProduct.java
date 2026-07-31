9f60e004ca61f41490b65409985a039d5c206912
QAEngineerBot <sachinpujari5579@gmail.com>
2026-07-31 15:51:58 +0530

    added faker class and createJobFakerUtil

--- src/test/java/com/api/request/model/CustomerProduct.java
+++ src/test/java/com/api/request/model/CustomerProduct.java
@@ -1,6 +1,7 @@
 package com.api.request.model;
 
-public record CustomerProduct( String dop,
+public record CustomerProduct( 
+     String dop,
      String serial_number,
      String imei1,
      String imei2,
